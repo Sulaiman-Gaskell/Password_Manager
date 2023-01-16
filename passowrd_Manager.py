7ad3f11f5c8b4c659e37659869260ccdbfc1c72d
Sulaiman-Gaskell <sulaimangaskell@gmail.com>
2023-01-16 19:42:26 +0000

    Create passowrd_Manager.py

---
+++ passowrd_Manager.py
@@ -0,0 +1,4 @@
+introText = '''V1.0.0
+Welcome to the Password Manager!
+Here you can store and generate passwords while checking the strength.
+Currently you can only check the strength of your password.'''
